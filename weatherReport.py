cdaaaea5be2a522e6a51d548c3f2e6bb56accc34
Hüseyin <husomacpro@192.168.1.21>
2026-02-08 15:49:47 +0300

    ilk yukleme

---
+++ weatherReport.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import requests
+
+url='http://api.openweathermap.org/data/2.5/weather'
+
+api_key='af2093eb5611ad6369cf17e35ce7587a'
+
+iconUrl='http://openweathermap.org/img/wn/{}@2x.png'
+
+def getWeather(city):
+   params= {'q':city,'appid':api_key,'lang':'tr'}
+   data = requests.get(url,params=params).json()
+   if data:
+        city=data['name'].capitalize()
+        country=data['sys']['country']
+        temp=int(data['main']['temp']-273.15)
+        icon=data['weather'][0]['icon']
+        condition=data['weather'][0]['description']
+        return(city,country,temp,icon,condition)
+
+def main():
+    city=cityEntry.get()
+    weather=getWeather(city)
+    if weather:
+        locationLabel['text']='{},{}'.format(weather[0],weather[1])
+        tempLabel['text'] ='{}°C'.format(weather[2])
+        conditionLabel['text']=weather[4]
+        icon=ImageTK.PhotoImage(Image.open(requests.get(iconUrl.format(weather[3]),stream=True).raw))
+        iconLabel.configure(image=icon)
+        iconLabel.image=icon
+
+app=Tk()
+app.geometry('300x450')
+app.title('Hava Durumu')
+
+cityEntry=Entry(app,justify='center')
+cityEntry.pack(fill=BOTH,ipady=10,padx=18,pady=5)
+cityEntry.focus()
+
+searchButton = Button(app,text='Arama',font=('arial',15),command=main)
+searchButton.pack(fill=BOTH,ipady=10,padx=20)
+
+iconLabel = Label(app)
+iconLabel.pack()
+
+locationLabel = Label(app,font=('Arial',40))
+locationLabel.pack()
+
+tempLabel = Label(app,font=('Arial',50,'bold'))
+tempLabel.pack()
+
+conditionLabel=Label(app,font=('Arial',20))
+conditionLabel.pack()
+
+app.mainloop()
